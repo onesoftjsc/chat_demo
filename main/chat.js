046d40a882dd08f360ff7768336fe22a7e1620ac
Quan Remote <hongquantc@gmail.com>
2017-06-05 18:27:34 +0700

    no message

--- main/chat.js
+++ main/chat.js
@@ -2,14 +2,14 @@
  * Created by phamquan on 6/4/17.
  */
 
-ibigfox.basePath="../../../";
-ibigfox.addPath('demo/chat/js/message/common/Tags.js', ['chat.message.common.Tags']);
+ibigfox.basePath="../../../"; // basePath + 'demo/chat/js/message/common/Tags.js' sẽ ra đường dẫn tương đối của file javascript so với chat.html
+ibigfox.addPath('demo/chat/js/message/common/Tags.js', ['chat.message.common.Tags']); // khai báo class nào ứng với file javascript nào
 ibigfox.addPath('demo/chat/js/message/cs/CSChat.js', ['chat.message.cs.CSChat']);
 ibigfox.addPath('demo/chat/js/message/item/ChatItem.js', ['chat.message.item.ChatItem']);
 ibigfox.addPath('demo/chat/js/message/sc/SCChat.js', ['chat.message.sc.SCChat']);
-ibigfox.resetBasePath();
+ibigfox.resetBasePath(); // reset lại basePath
 
-ibigfox.require('chat.message.common.Tags');
+ibigfox.require('chat.message.common.Tags'); // load file javascript ứng với class chat.message.common.Tags vào hệ thống
 ibigfox.require('chat.message.cs.CSChat');
 ibigfox.require('chat.message.item.ChatItem');
 ibigfox.require('chat.message.sc.SCChat');
@@ -19,19 +19,19 @@ ibigfox.require("BFConnect");
 var chat = chat || {};
 
 $(document).ready(function(){
-    chat.bfConnect = new BFConnect($("meta[name=serverLogic]").attr("content"), 10000);
+    chat.bfConnect = new BFConnect($("meta[name=serverLogic]").attr("content"), 10000); // khởi tạo một BFConnect để kết nối tới Server
 
-    chat.bfConnect.mapServiceToMessage[chatTags.CS_CHAT] = chat.message.cs.CSChat;
+    chat.bfConnect.mapServiceToMessage[chatTags.CS_CHAT] = chat.message.cs.CSChat; // map giữa service và class
     chat.bfConnect.mapServiceToMessage[chatTags.SC_CHAT] = chat.message.sc.SCChat;
 
-    chat.bfConnect.onDisconnected = function(){}
+    chat.bfConnect.onDisconnected = function(){} // khai báo các callback function
     chat.bfConnect.onStartNewSession = function(){}
 
-    chat.bfConnect.start();
+    chat.bfConnect.start(); // Bắt đầu thực hiện kết nối tới Server
 });
 
 chat.onclick = function(){
     var name = $('#name').val();
     var content = $('#content').val();
-    chat.bfConnect.send(new chat.message.cs.CSChat(name, content));
+    chat.bfConnect.send(new chat.message.cs.CSChat(name, content)); // gọi hàm send của BFConnect để gửi bản tin tới Server
 }
